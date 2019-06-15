ea34a6347f45014b65ef27ea10a5171ae81f6de5
liliang <dzliliang@126.com>
2019-06-15 10:03:06 +0800

    update

---
+++ src/main/java/com/lkker/msrabbitmq/model/SteelMillInformation.java
@@ -0,0 +1,74 @@
+package com.lkker.msrabbitmq.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ * @Author liliang
+ * @Date 2019/6/15 9:47
+ * @Description 钢厂信息
+ **/
+@Entity
+@Table(name = "steel_mill_information")
+public class SteelMillInformation {
+    // 钢厂id
+    @Column(name = "steel_id")
+    private String steelId;
+
+    // 钢厂名字
+    @Column(name = "steel_name")
+    private String steelName;
+
+    // 钢厂地址
+    private String address;
+
+    // 钢厂产量
+    private double yield;
+
+    // 钢厂创建时间
+    @Column(name = "creation_time")
+    private String creationTime;
+
+    @Id
+    public String getSteelId() {
+        return steelId;
+    }
+
+    public void setSteelId(String steelId) {
+        this.steelId = steelId;
+    }
+
+    public String getSteelName() {
+        return steelName;
+    }
+
+    public void setSteelName(String steelName) {
+        this.steelName = steelName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public double getYield() {
+        return yield;
+    }
+
+    public void setYield(double yield) {
+        this.yield = yield;
+    }
+
+    public String getCreationTime() {
+        return creationTime;
+    }
+
+    public void setCreationTime(String creationTime) {
+        this.creationTime = creationTime;
+    }
+}
